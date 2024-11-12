3ef8d71979ac55d7d19e6bcc435e8916e6ff9855
Kevin <valbuenatkevin@gmai.com>
2024-11-12 00:25:40 -0500

    Se agregan los métodos set y get a Tipopokemos
    Se agregan los métodos set y get a Tipopokemos

--- src/main/java/simulador/pokemon/Pokemon.java
+++ src/main/java/simulador/pokemon/Pokemon.java
@@ -5,6 +5,7 @@ public class Pokemon {
     private TipoPokemon tipo;
     private int salud;
     private int ataque;
+    
   public Pokemon(String nombre, TipoPokemon tipo, int salud,int ataque) {
     this.nombre = nombre;
     this.tipo = tipo;
@@ -13,35 +14,35 @@ public class Pokemon {
   }
 
   
-  public String getNombre() {
-    return nombre;
-}
+    public String getNombre() {
+        return nombre;
+    }
 
-public void setNombre(String nombre) {
-    this.nombre = nombre;
-}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-public TipoPokemon getTipo() {
-    return tipo;
-}
+    public TipoPokemon getTipo() {
+        return tipo;
+    }
 
-public void setTipo(TipoPokemon tipo) {
-    this.tipo = tipo;
-}
+    public void setTipo(TipoPokemon tipo) {
+        this.tipo = tipo;
+    }
 
-public int getSalud() {
-    return salud;
-}
+    public int getSalud() {
+        return salud;
+    }
 
-public void setSalud(int salud) {
-    this.salud = salud;
-}
+    public void setSalud(int salud) {
+        this.salud = salud;
+    }
 
-public int getAtaque() {
-    return ataque;
-}
+    public int getAtaque() {
+        return ataque;
+    }
 
-public void setAtaque(int ataque) {
-    this.ataque = ataque;
-}
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
 }
--- src/main/java/simulador/pokemon/TipoPokemon.java
+++ src/main/java/simulador/pokemon/TipoPokemon.java
@@ -1,7 +1,6 @@
 package simulador.pokemon;
 
 public class TipoPokemon {
-    public class TipoPokemon {
         private String tipoPrimario;
         private String tipoSecundario;
     
@@ -9,4 +8,22 @@ public class TipoPokemon {
             this.tipoPrimario = tipoPrimario;
             this.tipoSecundario = tipoSecundario;
         }
+
+     
+    public String getTipoPrimario() {
+        return tipoPrimario;
     }
+
+    public void setTipoPrimario(String tipoPrimario) {
+        this.tipoPrimario = tipoPrimario;
+    }
+
+    public String getTipoSecundario() {
+        return tipoSecundario;
+    }
+
+    public void setTipoSecundario(String tipoSecundario) {
+        this.tiposecundario = tipoSecundario;
+    }
+
+}
