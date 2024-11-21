5b8669da39d1c66f76f9fc88db1582ef90c192d1
nose <zorpresapecas@gmail.com>
2024-11-20 22:58:54 -0500

    se termino clase batalla

--- src/main/java/simulador/batalla/Batalla.java
+++ src/main/java/simulador/batalla/Batalla.java
@@ -1,28 +1,47 @@
 package simulador.batalla;
+
 import simulador.pokemon.Pokemon;
+import simulador.pokemon.Tipo;
+
 public class Batalla {
-    
-        private Pokemon pokemon1;
-        private Pokemon pokemon2;
-    
-        public Batalla(Pokemon pokemon1, Pokemon pokemon2) {
-            this.pokemon1 = pokemon1;
-            this.pokemon2 = pokemon2;
-        }
-    
-        public void iniciarBatalla() {
-            while (pokemon1.getSalud() > 0 && pokemon2.getSalud() > 0) {
-                pokemon1.atacar(pokemon2);
-                if (pokemon2.getSalud() > 0) {
-                    pokemon2.atacar(pokemon1);
-                }
+
+    private Pokemon pokemon1;
+    private Pokemon pokemon2;
+
+    public Batalla(Pokemon pokemon1, Pokemon pokemon2) {
+        this.pokemon1 = pokemon1;
+        this.pokemon2 = pokemon2;
+    }
+
+    public void iniciarBatalla() {
+        System.out.println("¡La batalla comienza entre " + pokemon1.getNombre() + " y " + pokemon2.getNombre() + "!");
+        System.out.println(pokemon1.getNombre() + " tiene " + pokemon1.getSalud() + " de salud.");
+        System.out.println(pokemon2.getNombre() + " tiene " + pokemon2.getSalud() + " de salud.");
+        
+        while (pokemon1.getSalud() > 0 && pokemon2.getSalud() > 0) {
+            double multiplicador = Tipo.obtenerMultiplicadorDeDaño(pokemon1.getTipoPrimario(), pokemon2.getTipoPrimario());
+            double danio = pokemon1.getAtaque() * multiplicador;
+            pokemon2.setSalud((int) (pokemon2.getSalud() - danio));
+            System.out.println(pokemon2.getNombre() + " ahora tiene " + pokemon2.getSalud() + " de salud.");
+
+            if (pokemon2.getSalud() > 0) {
+                multiplicador = Tipo.obtenerMultiplicadorDeDaño(pokemon2.getTipoPrimario(), pokemon1.getTipoPrimario());
+                danio = pokemon2.getAtaque() * multiplicador;
+                pokemon1.setSalud((int) (pokemon1.getSalud() - danio));
+                System.out.println(pokemon1.getNombre() + " ahora tiene " + pokemon1.getSalud() + " de salud.");
             }
-    
-            if (pokemon1.getSalud() <= 0) {
-                System.out.println(pokemon2.getNombre() + " gana la batalla");
-            } else {
-                System.out.println(pokemon1.getNombre() + " gana la batalla");
+            if (pokemon1.getSalud() > 0 && pokemon2.getSalud() > 0) {
+                System.out.println("El combate continúa...");
             }
+
         }
 
+        if (pokemon1.getSalud() <= 0) {
+            System.out.println(pokemon2.getNombre() + " gana la batalla");
+        } else {
+            System.out.println(pokemon1.getNombre() + " gana la batalla");
+        }
+    }
+
 }
+
